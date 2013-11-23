b16ab4565b0c63323ada0852aaf79bdfc40069f7
tcbeutler <tcbeutler@gmail.com>
2013-11-23 10:34:52 -0600

    require autocomplete now working

--- nodeComplete.py
+++ nodeComplete.py
@@ -3,20 +3,26 @@ import sublime
 import os
 
 ignoredDirectories = ['node_modules', 'swagger', 'generators']
+requireWordSeparators = "./\\\"-:,.;<>~!@#$%^&*|+=[]{}`~?"
 
 class NodeComplete(sublime_plugin.EventListener):
-
+    """
+    For "require" statements, add autocompletion of modules in current open folders.
+    This is to make relative paths much easier to require.
+    """
     def on_query_completions(self, view, prefix, locations):
-        currentFile = sublime.active_window().active_view().file_name()
-        #Only search on this drive.
-        folders = [folder for folder in sublime.active_window().folders() if folder[0] == currentFile[0]]
-        
-        #map returns a list of list, flatten this
-        files = [f for sublist in map(self.getFiles, folders) for f in sublist]
-
         if (prefix in 'require'):
-            relativePaths = ['require("{0}")'.format(os.path.relpath(fileName, currentFile).replace('\\', '/')) for fileName in files]
-            return [(rp, rp) for rp in relativePaths]
+
+            currentFile = sublime.active_window().active_view().file_name()
+            #Only search on this drive.
+            folders = [folder for folder in sublime.active_window().folders() if folder[0] == currentFile[0]]
+            
+            #map returns a list of lists, flatten this
+            files = [f for sublist in map(self.getFiles, folders) for f in sublist]
+
+            relativePaths = ["require('{0}')".format(os.path.relpath(fileName, currentFile)) for fileName in files]
+            return [self.formatQueryCompletionPath(rp) for rp in relativePaths]
+
         return []
 
     def getFiles(self, folder):
@@ -25,3 +31,10 @@ class NodeComplete(sublime_plugin.EventListener):
             subDirectories[:] = [directory for directory in subDirectories if directory not in ignoredDirectories]
             files = files + [os.path.join(rootDirectory, fileName) for fileName in directoryFiles if fileName[-3:] == '.js']
         return files
+
+    def formatQueryCompletionPath(self, path):
+        #Force forward slashes, remove extension and don't display '../' groups.
+        newPath = path.replace('\\', '/').replace('.js', '');
+        matchPath = newPath.replace('../', '')
+        return (matchPath, newPath)
+        
